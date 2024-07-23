177ab45426f6212539cda908a4d5cd586278970e
Alex Zajichek <alexzajichek@gmail.com>
2024-07-23 10:02:43 -0400

    Updated plot labels

--- OW_Tutorial/figure2.R
+++ OW_Tutorial/figure2.R
@@ -22,7 +22,7 @@ wt_distribution <-
   
   # Make weight bins and clean labels
   mutate(
-    Bins = Hmisc::cut2(Weight, cuts = c(seq(0, 1, .1), 1.25, 1.5, 2, 3, 5, 7, 10, 15, 21, 50)),
+    Bins = Hmisc::cut2(Weight, cuts = c(seq(0, 1, .1), 1.25, 1.5, 2, 3, 5, 7, 10, 15, 21, 50), formatfun = \(x) round(x, 1)),
     Group = 
       case_when(
         treated == 1 ~ "Off-pump",
@@ -69,7 +69,7 @@ wt_distribution <-
     legend.text = element_text(size = 14),
     axis.title = element_text(size = 14),
     axis.text = element_text(size = 12),
-    axis.text.x = element_text(size = 8, angle = 45),
+    axis.text.x = element_text(size = 10, angle = 45),
     strip.background = element_blank(),
     strip.text = element_text(size = 16, face = "bold", hjust = 0)
   ) +
